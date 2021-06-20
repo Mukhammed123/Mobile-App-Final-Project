2fa5a44ef4975ea433899f6d1ecb7113390e251f
Mukhammed123 <muhammedmusaaa@gmail.com>
2021-06-20 19:48:24 +0800

    First commit

---
+++ app/src/main/java/com/example/afinal/StudentModel.kt
@@ -0,0 +1,15 @@
+package com.example.afinal
+
+import java.util.*
+class StudentModel(
+    var id: Int = getAutoId(),
+    var name: String = "",
+    var email: String = ""
+) {
+    companion object {
+        fun getAutoId(): Int {
+            val random = Random()
+            return random.nextInt(100)
+        }
+    }
+}
